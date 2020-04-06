313c42f358142f850a8fd0ec355fc78f47c27bd2
Jayden <jayden.tavares@hotmail.com>
2020-04-06 19:52:25 +1000

    Camera Follow Script to follow target

---
+++ Assets/Scripts/Camera/MainCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+    public GameObject focus;
+    public float followSpeed = 0.2f;
+    
+    Vector3 position;
+    Vector3 offset = new Vector3(0, 0, -10);
+
+    void FixedUpdate() {
+        if (focus != null) {
+            transform.position = Vector3.Lerp(transform.position, focus.transform.position + offset, followSpeed);
+        }
+    }
+}
