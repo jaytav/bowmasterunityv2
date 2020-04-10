052625936a8cda0602416d0a8c94bcb3b8696a3c
Jayden <jayden.tavares@hotmail.com>
2020-04-10 12:54:42 +1000

    Interactables

---
+++ Assets/Scripts/Interact/Interactable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+    SpriteRenderer sprite;
+
+    void Start() {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    public void Highlight() {
+        sprite.enabled = true;
+    }
+
+    public void Unhighlight() {
+        sprite.enabled = false;
+    }
+
+    // Destroy Interactable held GameObject 
+    public void GetInteractedWith() {
+        Destroy(transform.parent.gameObject);
+    }
+}
---
+++ Assets/Scripts/Interact/Interactor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+// Can only focus 1 Interactable at a time
+public class Interactor : MonoBehaviour {
+    Interactable interactable;
+
+    void Update() {
+        if (this.interactable != null && Input.GetKeyDown(KeyCode.E)) {
+            this.interactable.GetInteractedWith();
+            this.interactable = null;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collider) {
+        Interactable interactable = collider.gameObject.GetComponent<Interactable>();
+
+        if (this.interactable == null && interactable != null) {
+            this.interactable = interactable;
+            interactable.Highlight();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider) {
+        Interactable interactable = collider.gameObject.GetComponent<Interactable>();
+
+        if (this.interactable != null && interactable != null) {
+            this.interactable = null;
+            interactable.Unhighlight();
+        }
+    }
+}
